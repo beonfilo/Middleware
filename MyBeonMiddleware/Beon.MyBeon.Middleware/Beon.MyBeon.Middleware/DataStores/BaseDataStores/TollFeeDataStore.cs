c1eac1669f48320d9dda0bc8c8e5e008d19633be
iyibiryazilim <00umutsonmez00@gmail.com>
2023-09-08 16:31:12 +0300

    trafficFee and TollFee eklendi.

---
+++ MyBeonMiddleware/Beon.MyBeon.Middleware/Beon.MyBeon.Middleware/DataStores/BaseDataStores/TollFeeDataStore.cs
@@ -0,0 +1,8 @@
+﻿using Beon.MyBeon.Middleware.Models;
+using Beon.MyBeon.Middleware.Services;
+
+namespace Beon.MyBeon.Middleware.DataStores.BaseDataStores;
+
+public class TollFeeDataStore : BaseDataStore<TollFee>,ITollFeeService
+{
+}
---
+++ MyBeonMiddleware/Beon.MyBeon.Middleware/Beon.MyBeon.Middleware/Models/Roadway.cs
@@ -0,0 +1,12 @@
+﻿using Beon.MyBeon.Middleware.Models.BaseModels;
+
+namespace Beon.MyBeon.Middleware.Models;
+
+public class Roadway : BaseModel
+{
+    public DateTime CreatedOn { get; set; }
+    //public ApplicationUser Owner;
+    public City? City { get; set; }
+    public County? County { get; set; }
+    public string Name{get; set;} = string.Empty;
+}
