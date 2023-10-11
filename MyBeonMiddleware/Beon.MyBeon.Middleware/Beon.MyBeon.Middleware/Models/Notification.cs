e5b829b97a664562a43bd7c98bc6830facf71b6b
Canberk Bekiroğlu <boom.canberk@gmail.com>
2023-10-11 15:28:44 +0300

    NotificationModel

---
+++ MyBeonMiddleware/Beon.MyBeon.Middleware/Beon.MyBeon.Middleware/Models/Notification.cs
@@ -0,0 +1,10 @@
+﻿namespace Beon.MyBeon.Middleware.Models
+{
+    public class Notification : BaseObject
+    {
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool? IsRead { get; set; }
+        public ApplicationUser? ApplicationUser { get; set; }
+    }
+}
