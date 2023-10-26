507a4fc8336cd308d262050a655e1ceeebdc72d2
Umut Sönmez <00umutsonmez00@gmail.com>
2023-10-26 12:21:55 +0300

    contract fix

--- MyBeonMiddleware/Beon.MyBeon.Middleware/Beon.MyBeon.Middleware/Models/Contract.cs
+++ MyBeonMiddleware/Beon.MyBeon.Middleware/Beon.MyBeon.Middleware/Models/Contract.cs
@@ -16,7 +16,39 @@ public class Contract : BaseModel
     public bool? IsActive { get; set; }
     public bool? IsCustomerContract { get; set; }
     public bool? IsHandoverProtocol { get; set; }
-    public DateTime HandevorProtocolOn { get; set; }
+    public DateTime? HandevorProtocolOn { get; set; }
     public ApplicationUser? Owner { get; set; }
+    public string? Status { get; set; }
+	public string StatusName
+	{
+		get
+		{
+
+			switch (Status)
+			{
+				case "SignatureState":
+					return "İmza Aşamasında";
+				case "IsValid ":
+					return "Yürürlükte";
+				case "Abrogated":
+					return "Feshedildi";
+				case "Terminated":
+					return "Süresi Doldu";
+				case "Cancelled":
+					return "İptal Edildi";
+				case "Devir":
+					return "Devir";
+				default:
+					return "Diğer";
+			}
+		}
+	}
+	public Currency? Currency { get; set; }
+	public Salesman? Salesman { get; set; }	
+	public ContractRevision? ContractRevision {  get; set; }	
+	public Quote? Quote { get; set; }
+	public Customer? HandoverProtocolCustomer { get; set; }
+	public Contract? HandoverProtocolContract { get; set; }
+	public FileData? CustomerContractFile { get; set; }
 
 }
