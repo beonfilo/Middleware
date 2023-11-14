6a57c21e1f6ca6888aebb2c67467706d0244fff7
Umut Sönmez <00umutsonmez00@gmail.com>
2023-11-14 16:38:01 +0300

    araç detayı ve fatura tamamlandı.

---
+++ MyBeonMiddleware/Beon.MyBeon.Middleware/Beon.MyBeon.Middleware/DataStores/OrderDocumentV2DataStore.cs
@@ -0,0 +1,9 @@
+﻿using Beon.MyBeon.Middleware.DataStores.BaseDataStores;
+using Beon.MyBeon.Middleware.Models;
+using Beon.MyBeon.Middleware.Services;
+
+namespace Beon.MyBeon.Middleware.DataStores;
+
+public class OrderDocumentV2DataStore : BaseDataStore<OrderDocumentV2>,IOrderDocumentV2Service
+{
+}
