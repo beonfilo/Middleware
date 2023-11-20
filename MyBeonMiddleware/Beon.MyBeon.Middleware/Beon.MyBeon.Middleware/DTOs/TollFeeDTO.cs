b68e1547c03ebce468c9596281531e79f7abc47e
Canberk Bekiroğlu <boom.canberk@gmail.com>
2023-11-20 15:49:22 +0300

    TollFeeDto update

--- MyBeonMiddleware/Beon.MyBeon.Middleware/Beon.MyBeon.Middleware/DTOs/TollFeeDTO.cs
+++ MyBeonMiddleware/Beon.MyBeon.Middleware/Beon.MyBeon.Middleware/DTOs/TollFeeDTO.cs
@@ -4,13 +4,20 @@ namespace Beon.MyBeon.Middleware.DTOs
 {
     public class TollFeeDTO
     {
-        public string? TollNumberDesc { get; set; }
-        public Guid? Vehicle { get; set; }
-        public Guid? Roadway { get; set; }
-        public string? EntryTollPointDesc { get; set; }
-        public string? ExitTollPointDesc { get; set; }
-        public DateTime? EntryDate { get; set; }
-        public DateTime? ExitDate { get; set; }
+		public string? TollNumberDesc { get; set; }
+		public Guid? Vehicle { get; set; }
+		public Guid? Roadway { get; set; }
+		public string? EntryTollPointDesc { get; set; }
+		public string? ExitTollPointDesc { get; set; }
+		public DateTime? EntryDate { get; set; }
+		public DateTime? ExitDate { get; set; }
+		public DateTime? TransactionOn { get; set; }
+		public Guid? ContractTransactionVehicle { get; set; }
+		public Guid? ContractTransaction { get; set; }
+		public Guid? Owner { get; set; }
+		public Guid? Customer { get; set; }
+		public double ChargeOutPrice { get; set; } = 0;
+		public int? ChargeOutStatus { get; set; } = 1;
 
-    }
+	}
 }
