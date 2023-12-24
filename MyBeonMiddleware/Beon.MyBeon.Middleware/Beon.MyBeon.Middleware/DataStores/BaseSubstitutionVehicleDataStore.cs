d2de1f6cbd9f105188e67b796e5fbafea9dfdad2
Volkan Sunuk <volkan.sunuk@iyibiryazilim.com.tr>
2023-12-24 20:04:45 +0300

    Add BaseSubstitutionVehicle object and upgrade version(1.1.59)

---
+++ MyBeonMiddleware/Beon.MyBeon.Middleware/Beon.MyBeon.Middleware/DataStores/BaseSubstitutionVehicleDataStore.cs
@@ -0,0 +1,9 @@
+﻿using Beon.MyBeon.Middleware.DataStores.BaseDataStores;
+using Beon.MyBeon.Middleware.Models.BaseModels;
+using Beon.MyBeon.Middleware.Services;
+
+namespace Beon.MyBeon.Middleware.DataStores;
+
+public class BaseSubstitutionVehicleDataStore : BaseDataStore<BaseSubstitutionVehicle>, IBaseSubstitutionVehicleService
+{
+}
